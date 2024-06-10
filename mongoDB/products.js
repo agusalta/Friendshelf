b6b06dc4206ac71ea3eadb3da349d5ab1cd71a41
agus <agusalta002@gmail.com>
2024-06-10 17:45:46 -0300

    upd agregué la propiedad pricing_plans, cambié el manifest.json y un estilo

--- mongoDB/products.js
+++ mongoDB/products.js
@@ -9,7 +9,7 @@ export async function insertProperties(db, col, jsonData) {
         const database = client.db(db);
         const collection = database.collection(col);
 
-        const { product_id, product_description, product_name, product_logo, rating, reviews, what_is, star_distribution, initial_reviews } = jsonData;
+        const { product_id, product_description, pricing_plans, product_name, product_logo, rating, reviews, what_is, star_distribution, initial_reviews } = jsonData;
 
         // Verificar si el ID ya existe en la base de datos
         const existingDocument = await collection.findOne({ product_id });
@@ -19,7 +19,7 @@ export async function insertProperties(db, col, jsonData) {
             return existingDocument;
         }
 
-        const document = { product_id, product_description, product_name, star_distribution, initial_reviews, product_logo, rating, reviews, what_is };
+        const document = { product_id, product_description, pricing_plans, product_name, star_distribution, initial_reviews, product_logo, rating, reviews, what_is };
         const result = await collection.insertOne(document);
         console.log(`Inserted document with ID: ${result.insertedId}`);
 
