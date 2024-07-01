3516717b5913f736e27b0a6db903d824bb22c7e4
agus <agusalta002@gmail.com>
2024-07-01 18:27:27 -0300

    update array --> set

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const logoURL = './assets/logo/light-logo-70.png'
+const logoURL = './assets/logo/light-logo-70.png';
 
 // Función para obtener nombres de productos desde la API
 async function fetchProductNamesFromAPI() {
@@ -19,19 +19,6 @@ async function fetchProductNamesFromAPI() {
     }
 }
 
-// Función para obtener nombres de productos desde almacenamiento local
-async function getProductNamesFromStorage() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get('productNames', result => {
-            if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError);
-            } else {
-                resolve(result.productNames || []);
-            }
-        });
-    });
-}
-
 // Función para almacenar nombres de productos en almacenamiento local
 async function setProductNamesToStorage(productNames) {
     return new Promise((resolve, reject) => {
@@ -45,34 +32,53 @@ async function setProductNamesToStorage(productNames) {
     });
 }
 
-// Función para resaltar nombres de productos en la página
-function highlightProductNames(productNames, logoURL) {
-    console.log('Ejecutando highlightProductNames');
-
-    const spans = document.querySelectorAll('span.VuuXrf');
-
-    spans.forEach(span => {
-        const productName = span.textContent.trim();
-        if (productNames.includes(productName) && !span.querySelector('img.product-logo')) {
-            console.log(`Insertando logo para producto: ${productName}`);
-
-            span.style.color = 'yellow';
-
-            const logoImg = document.createElement('img');
-            logoImg.src = logoURL;
-            logoImg.alt = 'Logo del producto'; 
-            logoImg.style.width = '50px'; o
-            logoImg.style.height = 'auto'; 
-            logoImg.className = 'product-logo'; 
-
-            // Añadir el logo directamente al span
-            span.appendChild(logoImg);
-        } else {
-            console.log(`El producto ${productName} ya tiene un logo.`);
-        }
+// Función para obtener nombres de productos desde almacenamiento local
+async function getProductNamesFromStorage() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get('productNames', result => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else {
+                resolve(result.productNames || []);
+            }
+        });
     });
 }
 
+// Función para resaltar nombres de productos en la página
+function highlightProductNames(productNamesArray, logoURL) {
+    const productNamesSet = new Set(productNamesArray);
+    
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const span = entry.target;
+                const productName = span.textContent.trim();
+
+                if (productNamesSet.has(productName)) {
+                    span.style.color = '#3a3ec7';
+                    span.style.fontWeight = 'bold';
+                }
+
+                // Una vez procesado, dejamos de observar este span
+                observer.unobserve(span);
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    const spans = document.querySelectorAll('span.VuuXrf');
+
+    spans.forEach(span => {
+        observer.observe(span);
+    });
+}
 
 // Función principal para obtener y resaltar nombres de productos
 async function fetchAndHighlightProductNames() {
@@ -81,36 +87,21 @@ async function fetchAndHighlightProductNames() {
 
         if (productNames.length === 0) {
             console.log('No product names fetched from storage, fetching from API...');
+            productNames = await fetchProductNamesFromAPI();
+            await setProductNamesToStorage(productNames);
         }
 
-        highlightProductNames(productNames);
+        highlightProductNames(productNames, logoURL);
 
     } catch (error) {
         console.error('Error fetching or highlighting product names:', error);
     }
 }
 
-// Función para agregar el logo al lado de la alerta
-function addLogoToAlert() {
-    const logoImg = document.createElement('img');
-    logoImg.src = logoURL;
-    logoImg.style.marginLeft = '10px'; // Ajusta el margen según sea necesario
-
-    const alertDiv = document.createElement('div');
-    alertDiv.style.display = 'flex';
-    alertDiv.style.alignItems = 'center';
-    alertDiv.appendChild(logoImg);
-
-    // Insertar el logo al principio del cuerpo del documento
-    document.body.insertBefore(alertDiv, document.body.firstChild);
-}
-
-// Ejecutar al cargar la página
-addLogoToAlert();
 fetchAndHighlightProductNames();
 document.addEventListener('DOMContentLoaded', fetchAndHighlightProductNames);
 
-// Utiliza MutationObserver para detectar cambios en el DOM y actualizar los resaltados
+// Detectar cambios en el DOM y actualizar los resaltados
 const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         if (mutation.type === 'childList') {
